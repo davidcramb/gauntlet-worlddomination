60b740bd0766e87771b8903b0cfdfe1a1a3489b7
Sandy Jones <sandynashv@gmail.com>
2016-04-19 20:40:12 -0500

    addition of weapons

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -29,3 +29,100 @@ var WarAxe = function() {
 };
 WarAxe.prototype = new Weapon();
 
+var Wand = function() {
+  this.name = "wand";
+  this.damage = 18;
+  this.hands = 2;
+};
+Wand.prototype = new Weapon();
+
+var Claymore = function() {
+  this.name = "claymore";
+  this.damage = 18;
+  this.hands = 2;
+};
+Claymore.prototype = new Weapon();
+
+var Mace = function() {
+  this.name = "mace";
+  this.damage = 18;
+  this.hands = 2;
+};
+Mace.prototype = new Weapon();
+
+var Shirken = function() {
+  this.name = "shirken";
+  this.damage = 18;
+  this.hands = 2;
+};
+Shirken.prototype = new Weapon();
+
+var Scythe = function() {
+  this.name = "scythe";
+  this.damage = 18;
+  this.hands = 2;
+};
+Scythe.prototype = new Weapon();
+
+var Sai = function() {
+  this.name = "sai";
+  this.damage = 5;
+  this.hands = 2;
+};
+Sai.prototype = new Weapon();
+
+var Katana = function() {
+  this.name = "katana";
+  this.damage = 5;
+  this.hands = 2;
+};
+Katana.prototype = new Weapon();
+
+var Tonfa = function() {
+  this.name = "tonfa";
+  this.damage = 5;
+  this.hands = 2;
+};
+Tonfa.prototype = new Weapon();
+
+var BrassKnuckles = function() {
+  this.name = "brass knuckles";
+  this.damage = 5;
+  this.hands = 2;
+};
+BrassKnuckles.prototype = new Weapon();
+
+var Potions = function() {
+  this.name = "potions";
+  this.damage = 5;
+  this.hands = 2;
+};
+Potions.prototype = new Weapon();
+
+var Taser = function() {
+  this.name = "taser";
+  this.damage = 5;
+  this.hands = 2;
+};
+Taser.prototype = new Weapon();
+
+var Staff = function() {
+  this.name = "staff";
+  this.damage = 5;
+  this.hands = 2;
+};
+Staff.prototype = new Weapon();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
