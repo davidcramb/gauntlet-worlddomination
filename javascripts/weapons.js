b09825ad7ae2a284934205d53a5b21489eaf2093
Sandy Jones <sandynashv@gmail.com>
2016-04-20 17:16:07 -0500

    hands, added weapon, altered damage

--- javascripts/weapons.js
+++ javascripts/weapons.js
@@ -10,64 +10,64 @@ var Weapon = function() {
 
 var Dagger = function() {
   this.name = "dagger";
-  this.damage = 4;
+  this.damage = 10;
   this.hands = 1;
 };
 Dagger.prototype = new Weapon();
 
 var BroadSword = function() {
   this.name = "broad sword";
-  this.damage = 14;
+  this.damage = 20;
   this.hands = 2;
 };
 BroadSword.prototype = new Weapon();
 
 var WarAxe = function() {
   this.name = "war axe";
-  this.damage = 18;
+  this.damage = 10;
   this.hands = 2;
 };
 WarAxe.prototype = new Weapon();
 
 var Wand = function() {
   this.name = "wand";
-  this.damage = 18;
-  this.hands = 2;
+  this.damage = 20;
+  this.hands = 1;
 };
 Wand.prototype = new Weapon();
 
 var Claymore = function() {
   this.name = "claymore";
-  this.damage = 18;
+  this.damage = 10;
   this.hands = 2;
 };
 Claymore.prototype = new Weapon();
 
 var Mace = function() {
   this.name = "mace";
-  this.damage = 18;
+  this.damage = 10;
   this.hands = 2;
 };
 Mace.prototype = new Weapon();
 
-var Shirken = function() {
+var Shuriken = function() {
   this.name = "shirken";
-  this.damage = 18;
-  this.hands = 2;
+  this.damage = 5;
+  this.hands = 1;
 };
 Shirken.prototype = new Weapon();
 
 var Scythe = function() {
   this.name = "scythe";
-  this.damage = 18;
-  this.hands = 2;
+  this.damage = 15;
+  this.hands = 1;
 };
 Scythe.prototype = new Weapon();
 
 var Sai = function() {
   this.name = "sai";
   this.damage = 5;
-  this.hands = 2;
+  this.hands = 1;
 };
 Sai.prototype = new Weapon();
 
@@ -81,39 +81,44 @@ Katana.prototype = new Weapon();
 var Tonfa = function() {
   this.name = "tonfa";
   this.damage = 5;
-  this.hands = 2;
+  this.hands = 1;
 };
 Tonfa.prototype = new Weapon();
 
 var BrassKnuckles = function() {
   this.name = "brass knuckles";
-  this.damage = 5;
+  this.damage = 10;
   this.hands = 2;
 };
 BrassKnuckles.prototype = new Weapon();
 
 var Potions = function() {
   this.name = "potions";
-  this.damage = 5;
+  this.damage = 15;
   this.hands = 2;
 };
 Potions.prototype = new Weapon();
 
 var Taser = function() {
   this.name = "taser";
-  this.damage = 5;
-  this.hands = 2;
+  this.damage = 20;
+  this.hands = 1;
 };
 Taser.prototype = new Weapon();
 
 var Staff = function() {
   this.name = "staff";
-  this.damage = 5;
-  this.hands = 2;
+  this.damage = 10;
+  this.hands = 1;
 };
 Staff.prototype = new Weapon();
 
-
+var ShrunkenHead = function() {
+  this.name = "shrunken head";
+  this.damage = 10;
+  this.hands = 1;
+};
+ShrunkenHead.prototype = new Weapon();
 
 
 
