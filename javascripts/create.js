c8f60a12ad37c3f14438b51f08da580fd449e0b6
complikatyed <katye.russell@gmail.com>
2016-04-21 21:23:36 -0500

    Add setWeapon function

--- javascripts/create.js
+++ javascripts/create.js
@@ -6,61 +6,23 @@ var Gauntlet = (function(originalGauntlet){
 
   var hero;
 
-  originalGauntlet.createHero = function() {
+  originalGauntlet.createHero = function(classId) {
     hero = new Gauntlet.Combatants.Human($('#player-name').val());
     hero.setClass(classId);
   };
 
-  originalGauntlet.setCharacterClass = function(classId) {
-
-    switch (classId) {
-      case "warrior":
-        console.log("Let's make a new warrior!");
-        var x = new originalGauntlet.GuildHall.Warrior();
-        break;
-      case "valkyrie":
-        console.log("Let's make a new valkyrie!");
-        var x = new originalGauntlet.GuildHall.Valkyrie();
-        break;
-      case "berserker":
-        console.log("Let's make a new berserker!");
-        var x = new originalGauntlet.GuildHall.Berserker();
-        break;
-      case "monk":
-        console.log("Let's make a new monk!");
-        var x = new originalGauntlet.GuildHall.Monk();
-        break;
-      case "wizard":
-        console.log("Let's make a new wizard!");
-        var x = new originalGauntlet.GuildHall.Wizard();
-        break;
-      case "sorcerer":
-        console.log("Let's make a new sorcerer!");
-        var x = new originalGauntlet.GuildHall.Sorcerer();
-        break;
-      case "conjurer":
-        console.log("Let's make a new conjurer!");
-        var x = new originalGauntlet.GuildHall.Conjurer();
-        break;
-      case "thief":
-        console.log("Let's make a new thief!");
-        var x = new originalGauntlet.GuildHall.Thief();
-        break;
-      case "ninja":
-        console.log("Let's make a new ninjna!");
-        var x = new originalGauntlet.GuildHall.Ninja();
-        break;
-      case "assassin":
-        console.log("Let's make a new assassin!");
-        var x = new originalGauntlet.GuildHall.Assassin();
-        break;
-      case "surprise":
-        console.log("Surprise me!");
-        generateClass();
+  originalGauntlet.setWeapon = function(weaponId) {
+    console.log("Weapon id is", weaponId);
+    if (weaponId === "Weapon") {
+      hero.setWeapon("bare hands");
     }
-    console.log(x);
-    //return x;
-  }
+    else {
+      hero.setWeapon(weaponId);
+    }
+
+    console.log(hero.toString());
+  };
+
 
  return originalGauntlet;
 
