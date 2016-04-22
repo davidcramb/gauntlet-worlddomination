dd23cf74871d058af5396a163717051ce06b5a7b
complikatyed <katye.russell@gmail.com>
2016-04-22 17:57:32 -0500

    Adjust structure of classId variable to make it global (so app.js can access it for createHero function)

--- javascripts/classListener.js
+++ javascripts/classListener.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var classId;
+var classId = "";
 var classLink = document.getElementsByClassName("class__link");
 var clickedThings = document.getElementsByClassName("selected");
 
@@ -14,10 +14,9 @@ var Gauntlet = (function(originalGauntlet){
 		};
 	};
 
-
 	originalGauntlet.markClicked = function(){
 		var clickedClass = event.target.closest('div');
-		var classId = clickedClass.getAttribute("id");
+		    classId = clickedClass.getAttribute("id");
 
 		if (clickedThings.length !== 0) {
 				clickedThings[0].classList.remove("selected");
@@ -26,19 +25,7 @@ var Gauntlet = (function(originalGauntlet){
 		else {
 				clickedClass.classList.add("selected");
 			}
-
-			Gauntlet.createHero(classId);
-
-
-
 	};
-
-	// originalGauntlet.setPlayerName = function () {
-	// 	let playerName = $('#player-name').val();
-	// 	Gauntlet.Combatants.Player.playerName = playerName;
-	// }
-
-	// $('.card__link[next=card--class]').click(Gauntlet.setPlayerName);
 
 
  return originalGauntlet;
