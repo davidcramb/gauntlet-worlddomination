96d77a430718994be285058a3050d902a6fc9022
Sandy Jones <sandynashv@gmail.com>
2016-04-21 19:34:16 -0500

    added classWeapon to DOMHandler

--- javascripts/DOMHandler1.js
+++ javascripts/DOMHandler1.js
@@ -4,7 +4,7 @@ let allWeapons = Object.keys(Gauntlet.Armory);
 for (let i = 0; i < allWeapons.length-11; i++) {
   let currentWeapon = allWeapons[i];
   $('#weapon-row').append(`<div class="col-sm-4">`);
-  $('#weapon-row').append(`<div class="card__button"><a class="btn btn--small btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
+  $('#weapon-row').append(`<div class="card__button classWeapon"><a class="btn btn--small btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
 }
 for (let i = 7; i < allWeapons.length; i++) {
   let currentWeapon = allWeapons[i];
