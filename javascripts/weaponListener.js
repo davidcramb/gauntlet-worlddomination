b282198f43a79be2d315ff35a36d0cf2dd864851
complikatyed <katye.russell@gmail.com>
2016-04-22 17:58:36 -0500

    Change variable structure similar to classListener.js, add comment

--- javascripts/weaponListener.js
+++ javascripts/weaponListener.js
@@ -1,22 +1,26 @@
 "use strict";
 
-var weaponId;
+var weaponId = "";
 var weaponLink = document.getElementsByClassName("weapon");
 var clickedStuff = document.getElementsByClassName("chosen");
 
+
 var Gauntlet = (function(originalGauntlet) {
 
+  // ----- Adds an event listener to each element with the class 'weapon' ----- //
 	originalGauntlet.addWeaponClick = function(currentWeapon) {
 
 		for (let i=0; i < weaponLink.length; i++) {
 				 var currentWeapon =  weaponLink[i];
+				// --- says 'call the weaponClicked function if the element gets clicked' --- //
 				currentWeapon.addEventListener("click", Gauntlet.weaponClicked);
 		};
 	};
 
+
 	originalGauntlet.weaponClicked = function(){
-		var clickedWeapon = event.target.closest('div');
-		var weaponId = clickedWeapon.getAttribute("id");
+		var clickedWeapon = event.target.closest("div");
+		    weaponId = clickedWeapon.getAttribute("id");
 
 		if (clickedStuff.length !== 0) {
 				clickedStuff[0].classList.remove("chosen");
@@ -25,8 +29,6 @@ var Gauntlet = (function(originalGauntlet) {
 		else {
 				clickedWeapon.classList.add("chosen");
 			}
-
-		Gauntlet.setWeapon(weaponId);
 	};
 
 
