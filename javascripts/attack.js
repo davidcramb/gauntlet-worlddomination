c77fedfc1bb49958c31e62757aa00c0fbe4bfb83
complikatyed <katye.russell@gmail.com>
2016-04-24 16:15:58 -0500

    Include display of battle stats and winner stats in function calls, remove logging

--- javascripts/attack.js
+++ javascripts/attack.js
@@ -1,36 +1,34 @@
 "use strict";
+
   var heroHealth;
   var enemyHealth;
+
   var Gauntlet = (function(originalGauntlet){
 
   originalGauntlet.setHealth = function(hero, enemy){
     heroHealth = hero.health;
     enemyHealth = enemy.health;
-    console.log("Hero Health set to ", hero.health)
-    console.log("Enemy Health set to", enemy.health)
+
   };
 
   originalGauntlet.attack = function (hero, enemy) {
     heroHealth -= enemy.weapon.damage;
     enemyHealth -= hero.weapon.damage;
-    console.log("Hero's health is now: ", heroHealth)
-    console.log("Enemy's health is now: ", enemyHealth)
-    originalGauntlet.checkHeroHP(hero);
-    originalGauntlet.checkEnemyHP(enemy);
+
+    originalGauntlet.checkHealth(heroHealth, enemyHealth);
   };
 
-  originalGauntlet.checkHeroHP = function (hero) {
+  originalGauntlet.checkHealth = function(hero, enemy) {
     if (heroHealth <= 0) {
-      console.log("The hero is defeated!")
+      Gauntlet.outputWinner("enemy");
       //run a new Game Over function that that either restarts the battle or takes player to the first page
-    }
-  }
-
-  originalGauntlet.checkEnemyHP = function (enemy) {
-    if (enemyHealth <= 0) {
-      console.log("The enemy is defeated!")
+    } else if (enemyHealth <= 0) {
+      Gauntlet.outputWinner("hero");
       //new function that asks if player wants to fight a new enemy or quit
+    } else {
+      Gauntlet.outputBattleStats(heroHealth, enemyHealth);
     }
-  }
+  };
+
   return originalGauntlet;
 }) (Gauntlet || {} );
