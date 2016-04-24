dbdcda28466860e3b35a5236303fb38e1f8ab860
complikatyed <katye.russell@gmail.com>
2016-04-24 16:15:14 -0500

    Add functions for display and update battle stats along with winner and end game banner

--- javascripts/stats.js
+++ javascripts/stats.js
@@ -1,4 +1,5 @@
 "use strict";
+
 var Gauntlet = (function(originalGauntlet){
 
 	originalGauntlet.outputHeroStats = function(hero) {
@@ -7,7 +8,7 @@ var Gauntlet = (function(originalGauntlet){
 		var heroStatString = "";
 		var capName = hero.playerName.toUpperCase();
 
-		heroStatString += `<div id="hero" class="hero_stats">`
+		heroStatString += `<div id="hero__stats" class="hero__stats">`
 		heroStatString += `<p>Welcome ${capName} the ${hero.class}.</p>`;
 		heroStatString += `<p>Your current health is ${hero.health}.</p>`;
 		heroStatString += `<p>We applaud your choice of ${hero.weapon},</p>`;
@@ -21,7 +22,7 @@ var Gauntlet = (function(originalGauntlet){
 		var startingEnemyStats = document.getElementById("enemy__stats");
 		var enemyStatString = "";
 
-		enemyStatString += `<div id="enemy" class="enemy_stats">`
+		enemyStatString += `<div id="enemy__stats" class="enemy__stats">`
 		enemyStatString += `<p>You must battle this ${enemy.species.name} ${enemy.class.name}</p>`;
 		enemyStatString += `<p>with health of ${enemy.health}.</p>`;
 		enemyStatString += `<p>Your foe will wield ${enemy.weapon}, `;
@@ -30,8 +31,37 @@ var Gauntlet = (function(originalGauntlet){
 		startingEnemyStats.innerHTML = enemyStatString;
 	};
 
+	originalGauntlet.outputBattleStats = function(heroHealth, enemyHealth) {
 
-	 return originalGauntlet;
+		var startingBattleStats = document.getElementById("battle__stats");
+		var battleStatString = "";
+
+		battleStatString += `<div id="battle__stats" class="battle__stats">`
+		battleStatString += `<p>Your health is now ${heroHealth}!</p>`;
+		battleStatString += `<p>Your foe's health is ${enemyHealth}!</p></div>`;
+
+		startingBattleStats.innerHTML = battleStatString;
+	};
+
+	originalGauntlet.outputWinner = function(winner) {
+    var winnerStats = document.getElementById("battle__stats");
+    var winnerString;
+
+    if (winner === "hero") {
+    	winnerString = "You have vanquished your foe!";
+    }
+    else {
+    	winnerString = "Sorry you're dead, loser.";
+    }
+
+    winnerStats.innerHTML = `<div class="battle__stats"> <h1>GAME OVER</h1>
+                              <p>${winnerString}</p></div>`;
+
+    // Here we need to add a "play again" button instead of the "attack" button
+
+	};
+
+	return originalGauntlet;
 
 }) (Gauntlet || {});
 
