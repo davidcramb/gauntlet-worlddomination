d25de0a408b2b998f5ade5d81c4f0fb4b6c5e303
David Cramb <david.cramb@gmail.com>
2016-04-24 20:25:13 -0500

    Code cleanup: Removed redundant instances of DOM manipulation from stats.js

--- javascripts/stats.js
+++ javascripts/stats.js
@@ -8,11 +8,11 @@ var Gauntlet = (function(originalGauntlet){
 		var heroStatString = "";
 		var capName = hero.playerName.toUpperCase();
 
-		heroStatString += `<div id="hero__stats" class="hero__stats">`
-		heroStatString += `<p>Welcome ${capName} the ${hero.class}.</p>`;
-		heroStatString += `<p>Your current health is ${hero.health}.</p>`;
-		heroStatString += `<p>We applaud your choice of ${hero.weapon},</p>`;
-    heroStatString += `<p>providing ${hero.weapon.damage} points of damage.</p></div>`;
+		heroStatString += `<div id="hero__stats" class="hero__stats">
+										   <p>Welcome ${capName} the ${hero.class}.</p>
+ 	 									   <p>Your current health is ${hero.health}.</p>
+										   <p>We applaud your choice of ${hero.weapon},</p>
+								       <p>providing ${hero.weapon.damage} points of damage.</p></div>`
 
 		startingHeroStats.innerHTML = heroStatString;
 	};
@@ -22,11 +22,11 @@ var Gauntlet = (function(originalGauntlet){
 		var startingEnemyStats = document.getElementById("enemy__stats");
 		var enemyStatString = "";
 
-		enemyStatString += `<div id="enemy__stats" class="enemy__stats">`
-		enemyStatString += `<p>You must battle this ${enemy.species.name} ${enemy.class.name}</p>`;
-		enemyStatString += `<p>with health of ${enemy.health}.</p>`;
-		enemyStatString += `<p>Your foe will wield ${enemy.weapon}, `;
-		enemyStatString += `which carries ${enemy.weapon.damage} points of damage.</p></div>`;
+		enemyStatString += `<div id="enemy__stats" class="enemy__stats">
+		  									<p>You must battle this ${enemy.species.name} ${enemy.class.name}</p>
+		  									<p>with health of ${enemy.health}.</p>
+											  <p>Your foe will wield ${enemy.weapon}, 
+											  which carries ${enemy.weapon.damage} points of damage.</p></div>`;
 
 		startingEnemyStats.innerHTML = enemyStatString;
 	};
@@ -36,9 +36,9 @@ var Gauntlet = (function(originalGauntlet){
 		var startingBattleStats = document.getElementById("battle__stats");
 		var battleStatString = "";
 
-		battleStatString += `<div id="battle__stats" class="battle__stats">`
-		battleStatString += `<p>Your health is now ${heroHealth}!</p>`;
-		battleStatString += `<p>Your foe's health is ${enemyHealth}!</p></div>`;
+		battleStatString += `<div id="battle__stats" class="battle__stats">
+												 <p>Your health is now ${heroHealth}!</p>
+												 <p>Your foe's health is ${enemyHealth}!</p></div>`
 
 		startingBattleStats.innerHTML = battleStatString;
 	};
