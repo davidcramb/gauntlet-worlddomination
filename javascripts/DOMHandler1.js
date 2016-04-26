601ebeb06b50ac16d24a02bc8d2c41c008b83414
complikatyed <katye.russell@gmail.com>
2016-04-26 13:26:37 -0500

    Add feature to jump to focus when 'class' and 'weapon' buttons are clicked

--- javascripts/DOMHandler1.js
+++ javascripts/DOMHandler1.js
@@ -6,13 +6,13 @@ for (let i = 0; i < 3; i++) {
 }
 for (let i =0; i < 6; i++) {
   let currentWeapon = allWeapons[i];
-  $('#weapon-col0').append(`<div id="${currentWeapon}" class="card__button btn--med weapon"><a class="btn btn--small btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
+  $('#weapon-col0').append(`<div id="${currentWeapon}" class="card__button weapon"><a class="btn btn--small btn--blue" href="#buildHero"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
 }
 for (let i = 6; i < 12; i++) {
   let currentWeapon = allWeapons[i];
-  $('#weapon-col1').append(`<div id="${currentWeapon}" class="card__button btn--med weapon"><a class="btn btn--small btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
+  $('#weapon-col1').append(`<div id="${currentWeapon}" class="card__button weapon"><a class="btn btn--small btn--blue" href="#buildHero"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
 }
 for (let i = 12; i < allWeapons.length; i++) {
   let currentWeapon = allWeapons[i];
-  $('#weapon-col2').append(`<div id="${currentWeapon}" class="card__button btn--med weapon"><a class="btn btn--small btn--blue" href="#"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
+  $('#weapon-col2').append(`<div id="${currentWeapon}" class="card__button weapon"><a class="btn btn--small btn--blue" href="#buildHero"><span class="btn__prompt">></span><span class="btn__text">${currentWeapon}</span></a></div>`)
 }
